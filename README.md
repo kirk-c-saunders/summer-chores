de90003e1b61ab29cec4b5a44867fad68f2dd4ae
Kirk Saunders <torikirksaunders@gmail.com>
2026-02-02 21:03:03 -0500

    finalizing changest for both javascript and readme

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # Summer Chores
 - **Description**: A javascript file that follows someone working through a list of chores.
 - **Tool Requirements**: Requires Node.js
-- **Starting Program**: Simply run `node callbackVersion.js` in an appropriately scoped terminal (either navigated to the project folder, or an intigrated terminal in the IDE)
+- **Starting Program**: Simply:
+    1. Run `node callbackVersion.js` in an appropriately scoped terminal (either navigated to the project folder, or an intigrated terminal in the IDE)
+    2. When asked, provide the name of the person who is completing the chores.
+- **Chore List and Rules** - below is the full list of chores and the time it takes to complete each item.
+    1. Mowing the yard - 2 Seconds
+    1. Weed eating the yard - 1.5 Seconds
+    1. Trim the hedges - 1 Second
+    1. Collect wood - 2.5 Seconds
+    1. Water the garden - .5 Seconds
--- callbackVersion.js
+++ callbackVersion.js
@@ -1,4 +1,5 @@
 const readline = require('node:readline');
+let fellAsleep = false;
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -6,6 +7,81 @@ const rl = readline.createInterface({
 });
 
 rl.question(`What's the name of the person completing chores?\n`, name => {
-  console.log(`Hi ${name}!`);
   rl.close();
+  doSummerChores(name);
 });
+
+function doSummerChores(name){
+  mowYard(name, () => {
+    weedEat(name, () => {
+      trimHedges(name, () => {
+        collectWood(name, () => {
+          waterGarden(name, () => {
+            if(!fellAsleep) {
+              console.log(`${name} finished all their chores!`);
+            }
+          })
+        })
+      })
+    })
+  })
+}
+
+function mowYard (name, callback){
+  setTimeout(() => {
+    console.log(`${name} mowed the yard.`);
+    callback();
+  }, 2000);
+}
+
+function weedEat(name, callback){
+  setTimeout(() => {
+    if (Math.random() <= .6){ //60% chance of completing the task
+      console.log(`${name} finished using the weed eater.`);
+      callback();
+    }
+    else{
+      console.log(`${name} fell asleep after mowing the yard.`);
+      fellAsleep = true;
+    }
+  }, 1500);   
+}
+
+function trimHedges(name, callback){
+  setTimeout(() => {
+    if (Math.random() <= .6){ //60% chance of completing the task
+      console.log(`${name} finished trimming the hedges.`);
+      callback();
+    }
+    else{
+      console.log(`${name} fell asleep after weed eating the yard.`);
+      fellAsleep = true;
+    }
+  }, 1000);   
+}
+
+function collectWood(name, callback){
+  setTimeout(() => {
+    if (Math.random() <= .6){ //60% chance of completing the task
+      console.log(`${name} finished collecting wood.`);
+      callback();
+    }
+    else{
+      console.log(`${name} fell asleep after trimming the hedges.`);
+      fellAsleep = true;
+    }
+  }, 2500);   
+}
+
+function waterGarden(name, callback){
+  setTimeout(() => {
+    if (Math.random() <= .6){ //60% chance of completing the task
+      console.log(`${name} finished watering the garden.`);
+      callback();
+    }
+    else{
+      console.log(`${name} fell asleep after collecting wood.`);
+      fellAsleep = true;
+    }
+  }, 500);   
+}
